c499ef2cfa7edd7dbe86728be0c566d04d6069f5
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-05-23 15:16:46 +0530

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi";
+import { AiFillPlusCircle } from "react-icons/ai";
 
 
 const App = () => {
+
+  const [contacts, setContacts] = useState([]);
+
+
   return (
-    <div>App</div>
+    <div className="mx-auto max-w-[370px] px-4">
+      <Navbar />
+      <div className="flex gap-2">
+      <div className="flex relative flex-grow items-center">
+      <FiSearch className="ml-1 text-white absolute text-3xl"/>
+
+        <input 
+        type="text" 
+        className="h-10 pl-9 flex-grow text-white rounded-md border border-white bg-transparent " />
+      </div>
+      <AiFillPlusCircle className="text-5xl cursor-pointer text-white "/>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+const Navbar = () => {
+  return (
+    <div className="my-4 gap-2 h-[60px] rounded-lg text-xl font-medium bg-white flex items-center justify-center">
+        <img src="/firebase.svg" alt="" />
+        <h1>Firebase Contact App</h1>
+    </div>
+  )
+}
+
+export default Navbar
